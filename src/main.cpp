849c31b2c0387e8dfb63ba7bf268411777a4fbe0
wozprzemek <wozprzemek@gmail.com>
2022-08-21 20:43:49 +0200

    Working version. Fix parameters

--- src/main.cpp
+++ src/main.cpp
@@ -62,23 +62,27 @@ public:
     double Lk;
     Constants c;
 
-    Ant(/*int current_oligo*/ int n) {
-        this->current_oligo_index = 0; //setting the starting point as 1 (for now) ->[spawn_ants]
-        this->path.push_back(0);
+    Ant(int oligo_count, int starting_index) {
+        this->current_oligo_index = starting_index; //setting the starting point as 1 (for now) ->[spawn_ants]
+        this->path.push_back(starting_index);
 
         //filling the not_visited set, starting from the point after 1 (for now) ->[spawn_ants]
-        for (int i = 1; i < n; i++) {
-            not_visited.insert(i);
+        for (int i = 0; i < oligo_count; i++) {
+            if (i != starting_index) {
+                not_visited.insert(i);
+            }
         }
     }
 
-    void reset(int n) {
-        this->current_oligo_index = 0;
+    void reset(int oligo_count, int starting_index) {
+        this->current_oligo_index = starting_index;
         this->path.clear();
         this->path.push_back(current_oligo_index);
         this->not_visited.clear();
-        for (int i = 1; i < n; i++) {
-            not_visited.insert(i);
+        for (int i = 0; i < oligo_count; i++) {
+            if (i != starting_index) {
+                not_visited.insert(i);
+            }
         }
 
     }
@@ -131,26 +135,25 @@ public:
 
 int overlap(std::string a, std::string b, int len) {
     int overlap_score = len;
+    // cout << "FUN overlap: " << a << ", " << b << " " << len << endl;
     for (int i=1; i<len+1; i++) {
-        std::string a_start =  a.substr(0, i);
-        std::string b_end = b.substr(len - i, i);
+        // std::string a_start =  a.substr(0, i);
+        // std::string b_end = b.substr(len - i, i);
 
         std::string a_end =  a.substr(len - i, i);
         std::string b_start = b.substr(0, i);
 
-        if (a_start == b_end || a_end == b_start) {
-            if (len - i < overlap_score) {
-                overlap_score = len - i;
-            }  // lower overlap_score == bigger overlap
+        if (a_end == b_start) {
+            overlap_score = len - i; // lower overlap_score == bigger overlap
         }
     }
     
     return overlap_score;
 }
 
-double calculate_lk(Ant ant, vector<vector<int>> overlaps, int n) {
+double calculate_lk(Ant ant, vector<vector<int>> overlaps, int oligo_count) {
     float Lk = 0;
-    for (int i = 1; i < n + 1; i++) {
+    for (int i = 1; i < oligo_count; i++) {
         Lk += overlaps[ant.path[i - 1]][ant.path[i]];
         // cout << point_distances[i-1][i] << " ";
     }
@@ -158,9 +161,9 @@ double calculate_lk(Ant ant, vector<vector<int>> overlaps, int n) {
     return Lk;
 }
 
-vector<vector<double>> update_trail_levels(Ant ant, double Lk, vector<vector<double>> T, Constants c, int n) {
+vector<vector<double>> update_trail_levels(Ant ant, double Lk, vector<vector<double>> T, Constants c, int oligo_count) {
     vector<vector<double>> pheromone_levels = T;
-    for (int i = 1; i < n + 1; i++) { //add the pheromones along the ant's path
+    for (int i = 1; i < oligo_count; i++) { //add the pheromones along the ant's path
         pheromone_levels[ant.path[i - 1]][ant.path[i]] += c.Q / Lk;
     }
 
@@ -168,11 +171,11 @@ vector<vector<double>> update_trail_levels(Ant ant, double Lk, vector<vector<dou
 }
 
 
-vector<vector<double>> evaporate_trail_levels(vector<vector<double>> T, Constants c, int n) {
+vector<vector<double>> evaporate_trail_levels(vector<vector<double>> T, Constants c, int oligo_count) {
     vector<vector<double>> pheromone_levels = T;
 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
+    for (int i = 0; i < oligo_count; i++) {
+        for (int j = 0; j < oligo_count; j++) {
             pheromone_levels[i][j] = pheromone_levels[i][j] * c.Ro;  //evaporate the pheromones on all possible paths
         }
     }
@@ -181,13 +184,13 @@ vector<vector<double>> evaporate_trail_levels(vector<vector<double>> T, Constant
 }
 
 
-vector<vector<double>> calculate_visibilities(vector<Oligo> oligos, int n, int k) {
+vector<vector<double>> calculate_visibilities(vector<Oligo> oligos, int oligo_count, int k) {
     vector<vector<double>> visibilities;
     vector<double> row;
     double visibility, overlap_tmp;
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < oligo_count; i++) {
         row.clear();
-        for (int j = 0; j < n; j++) {
+        for (int j = 0; j < oligo_count; j++) {
             overlap_tmp = overlap(oligos[i].sequence, oligos[j].sequence, k);
             if (overlap_tmp != 0) { //if distance is 0, set visibility as -1
                 visibility = 1 / overlap_tmp;
@@ -204,15 +207,17 @@ vector<vector<double>> calculate_visibilities(vector<Oligo> oligos, int n, int k
 }
 
 
-vector<vector<int>> calculate_overlaps(vector<Oligo> oligos, int n, int k) {
+vector<vector<int>> calculate_overlaps(vector<Oligo> oligos, int oligo_count, int k) {
+    // cout << "FUN calculate_overlaps" << endl;
     vector<vector<int>> overlaps;
     vector<int> row;
     int overlap_tmp;
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < oligo_count; i++) {
         row.clear();
-        for (int j = 0; j < n; j++) {
+        for (int j = 0; j < oligo_count; j++) {
+            // cout << i << " " << j << endl;
             overlap_tmp = overlap(oligos[i].sequence, oligos[j].sequence, k);
-            cout << oligos[i].sequence << " " << oligos[j].sequence << " " << overlap_tmp << endl;
+            // cout << oligos[i].sequence << " " << oligos[j].sequence << " " << overlap_tmp << endl;
             row.push_back(overlap_tmp);
         }
         overlaps.push_back(row);
@@ -234,49 +239,89 @@ vector<vector<double>> initialize_pheromones(int n) {
     return pheromones;
 }
 
-vector<Ant> spawn_ants(int n) {
+vector<Ant> spawn_ants(int oligo_count, int starting_oligo_index) {
     vector<Ant> ants;
-    for (int i = 0; i < n; i++) {
-        Ant ant(n);
+    for (int i = 0; i < oligo_count; i++) {
+        Ant ant(oligo_count, starting_oligo_index);
         ants.push_back(ant);
     }
     return ants;
 }
 
+string join_sequence(vector<Oligo> final_spectrum, int len) {
+    string final_sequence = final_spectrum[0].sequence;
+    for (int i=0; i < final_spectrum.size() - 1; i++) {
+        int overlap_score = overlap(final_spectrum[i].sequence, final_spectrum[i+1].sequence, len);
+        // final_sequence.append(final_spectrum[i].sequence);
+        final_sequence.append(final_spectrum[i+1].sequence.substr(len - overlap_score, overlap_score));
+    }
+
+    return final_sequence;
+}
+
+int find_starting_oligo(string oligo,vector<Oligo> spectrum) {
+    for (int i=0; i<spectrum.size(); i++) {
+        if (spectrum[i].sequence == oligo) {
+            return spectrum[i].index;
+        }
+    }
+    return 0;
+}
+
+// double calculate_accuracy(string start_sequence, string final_sequence) {
+//     for 
+// }
 
 int main(int argc, char * argv[]) {
 
     srand(time(NULL));
     double minimum = 1000000;
-    int iteration_number = 100;
+    int iteration_number = 200;
     vector<int> path;
     string filename = argv[1];
     double Lk;
     Constants c;
     vector<int> shortest_path;
+    string start_sequence;
+    string starting_oligo;
+    int starting_oligo_index;
 
     // read parameters
     ifstream infile(filename);
     int n, k, index = 0;
-    int ant_number = n;
     infile >> n;
     infile >> k;
-    
+    infile >> start_sequence;
+
+
+    int oligo_count = n - k + 1;
+    int ant_number = oligo_count;
+
     // read oligos
     vector<Oligo> spectrum;
+    vector<Oligo> final_spectrum;
+
     string sequence;
     while (infile >> sequence){
         spectrum.push_back(Oligo(index, sequence));
         index++;
     }
 
-    vector<vector<int>> oligo_overlaps = calculate_overlaps(spectrum, n, k); //initialize oligo overlap table
-    vector<vector<double>> oligo_visibilities = calculate_visibilities(spectrum, n, k); //initialize oligo visibilities table
+    starting_oligo = start_sequence.substr(0, k);
+    starting_oligo_index = find_starting_oligo(starting_oligo, spectrum);
+
+    // cout << "INITIALIZATION START" << endl;
+    vector<vector<int>> oligo_overlaps = calculate_overlaps(spectrum, oligo_count, k); //initialize oligo overlap table
+    vector<vector<double>> oligo_visibilities = calculate_visibilities(spectrum, oligo_count, k); //initialize oligo visibilities table
     vector<vector<double>> T = initialize_pheromones(n); //initialize pheromones table
 
-    // /* ------test oligo visibilities------ */
-    // for (int i=0; i<n; i++){
-    //     for (int j=0; j<n; j++){
+    // cout << "INITIALIZATION SUCCESSFUL" << endl;
+    // cout << "n: " << n << endl;
+    // cout << "spectrum size: " << spectrum.size() << endl;
+
+    /* ------test oligo visibilities------ */
+    // for (int i=0; i<oligo_count; i++){
+    //     for (int j=0; j<oligo_count; j++){
     //         cout << oligo_visibilities[i][j] << " ";
     //     }
     //     cout << endl;
@@ -285,52 +330,59 @@ int main(int argc, char * argv[]) {
     /* ------test oligo overlaps------ */
     // cout << oligo_overlaps[0].size() << endl;
 
-    // for (int i=0; i<n; i++){
-    //     for (int j=0; j<n; j++){
+    // for (int i=0; i<oligo_count; i++){
+    //     for (int j=0; j<oligo_count; j++){
     //         cout << oligo_overlaps[i][j] << " ";
     //     }
-    //     cout << endl << endl;
+    //     cout << endl;
     // }
 
-
     // /* ------test pheromones------ */
-    // for (int i=0; i<n; i++){
-    //     for (int j=0; j<n; j++){
+    // for (int i=0; i<oligo_count; i++){
+    //     for (int j=0; j<oligo_count; j++){
     //         cout << T[i][j] << " ";
     //     }
     //     cout << endl;
     // }
 
-    vector<Ant> ants = spawn_ants(n); //spawn all ants
+    vector<Ant> ants = spawn_ants(ant_number, starting_oligo_index); //spawn all ants
+
+    // cout << "ANTS SPAWNED" << endl;
 
     //MAIN LOOP
     for (int t = 0; t < iteration_number; t++) {
-        cout << "Iteration: " << t << endl;
+        cout << "ITERATION " << t << endl;
 
         // MOVE LOOP
-        for (int oligo = 0; oligo < n; oligo++) { //iterate over cities
+        for (int oligo = 0; oligo < oligo_count; oligo++) { //iterate over cities
             for (int l = 0; l < ant_number; l++) { //iterate over ants
                 ants[l].move(T, oligo_visibilities); //move each ant
             }
         }
 
+        // cout << "   ANTS MOVED" << endl;
+
         // RESET AND UPDATE LOOP
         for (int l = 0; l < ant_number; l++) { //iterate over ants
-            ants[l].path.push_back(0); //return to spawn oligo
-            Lk = calculate_lk(ants[l], oligo_overlaps, n); //calculate the total distance for ant
-            T = update_trail_levels(ants[l], Lk, T, c, n); //add the pheromones along the ant's path
+            Lk = calculate_lk(ants[l], oligo_overlaps, oligo_count); //calculate the total distance for ant
+            T = update_trail_levels(ants[l], Lk, T, c, oligo_count); //add the pheromones along the ant's path
+            // cout << Lk << " ";
             if (Lk < minimum) {
                 minimum = Lk; //check if the path is the shortest yet
-                // shortest_path = ants[l].path;
+                shortest_path = ants[l].path;
             }
-            ants[l].reset(n);
+            ants[l].reset(oligo_count, starting_oligo_index);
 
         }
-        T = evaporate_trail_levels(T, c, n); //evaporate all pheromones
+
+        // cout << "   TRAILS UPDATED" << endl;
+
+        T = evaporate_trail_levels(T, c, oligo_count); //evaporate all pheromones
+
+        // cout << "   TRAILS EVAPORATED" << endl;
+        // cout << "ITERATION " << t << "END" << endl;
+        // cout << endl;
         // cout << "Shortest path: ";
-        // for (int i=0; i<n; i++){
-        //     cout << shortest_path[i] << " ";
-        // }
         // cout << endl;
         /* ------test pheromones------ */
         // for (int i = 0; i < n; i++) {
@@ -341,6 +393,17 @@ int main(int argc, char * argv[]) {
         // }
     }
     cout << "Minimum: " << minimum << endl;
+    for (int i=0; i<oligo_count; i++){
+        cout << spectrum[shortest_path[i]].sequence << " ";
+        final_spectrum.push_back(spectrum[shortest_path[i]]);
+    }
+
+    string final_sequence = join_sequence(final_spectrum, k);
+
+    cout << endl << endl;
+    cout << start_sequence << endl;
+    cout << final_sequence << endl;
+
 }
 
 
--- tools/instance_generator.py
+++ tools/instance_generator.py
@@ -2,11 +2,10 @@ import random
 import copy
 
 NUCLEOTIDES = ['A', 'C', 'G', 'T']
-N = 100 # sequence length
+N = 50 # sequence length
 K = 4 # slice length
 NEG = int(0.06 * (N-K+1)) # negative error percentage
-POS = int(0.1 * (N-K+1)) # positive error percentage
-
+POS = int(0.06 * (N-K+1)) # positive error percentage
 
 def generate_sequence():
     return [random.choice(NUCLEOTIDES) for i in range(N)]
@@ -16,10 +15,11 @@ def generate_spectrum(sequence):
     return [''.join(sequence[i:K+i]) for i in range(N-K+1)]
 
 
-def write_to_file(filename, n, k, spectrum):
+def write_to_file(filename, n, k, spectrum, sequence):
     with open(filename, 'w') as file:
         file.write(str(n) + '\n')
         file.write(str(k) + '\n')
+        file.write(''.join(sequence) + '\n')
         for v in spectrum:
             file.write(v + '\n')
 
@@ -29,6 +29,8 @@ def apply_negative_error(spectrum, neg):
     new_spectrum = copy.deepcopy(spectrum)
     random.shuffle(new_spectrum)
     # apply negative error
+    if neg == 0:
+        return new_spectrum
     return new_spectrum[:-neg]
 
 
@@ -44,8 +46,7 @@ def apply_positive_error(spectrum, pos):
 
 sequence = generate_sequence()
 spectrum = generate_spectrum(sequence)
-
 spectrum = apply_negative_error(spectrum, NEG)
 spectrum = apply_positive_error(spectrum, POS)
 
-write_to_file('../src/input.txt', N, K, spectrum)
+write_to_file('../src/input.txt', N, K, spectrum, sequence)
